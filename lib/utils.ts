d32dc7d60b10c8b5dbce1ef44d9362616f52a724
Roberth González <reliutg@gmail.com>
2025-01-19 15:26:53 +0100

    Add generateId utility function for unique identifier generation

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function generateId() {
+  return Math.random().toString(36).substring(2, 15);
+} 
+
 export function getRandomColor() {
   const colors = [
     '#fef3c7', // Yellow
